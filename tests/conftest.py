4016c39f00e2259a4b8fdd8b0169951d54797708
Sami Jaghouar <sami.jaghouar@gmail.com>
2025-02-25 01:29:53 +0000

    add seqlen to tests
    Signed-off-by: Sami Jaghouar <sami.jaghouar@gmail.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,10 +8,10 @@ import pytest
 from zeroband.training.data import STABLE_FILE
 
 
-def _create_one_pa_file(file_name: str, batch_size: int = 1):
+def _create_one_pa_file(file_name: str, batch_size: int, seq_len: int):
     # Initialize lists for each column
-    input_tokens_list = [[1] * 10 for _ in range(batch_size)]  # Wrap in list
-    output_tokens_list = [[1] * 10 for _ in range(batch_size)]  # Wrap in list
+    input_tokens_list = [[1] * seq_len for _ in range(batch_size)]  # Wrap in list
+    output_tokens_list = [[1] * seq_len for _ in range(batch_size)]  # Wrap in list
     advantages_list = [1] * batch_size
     proofs_list = [b"I am toploc proof, handcrafted by jack"] * batch_size
     steps_list = [0] * batch_size
@@ -31,13 +31,13 @@ def _create_one_pa_file(file_name: str, batch_size: int = 1):
     pq.write_table(table, file_name)
 
 
-def _create_fake_rollout_parquet_file(path: Path, steps: list[int], num_files: int, batch_size: int):
+def _create_fake_rollout_parquet_file(path: Path, steps: list[int], num_files: int, batch_size: int, seq_len: int):
     for s in steps:
         step_path = path / f"step_{s}"
 
         for i in range(num_files):
             os.makedirs(step_path, exist_ok=True)
-            _create_one_pa_file(step_path / f"{i}.parquet", batch_size)
+            _create_one_pa_file(step_path / f"{i}.parquet", batch_size, seq_len)
 
         stable_file = step_path / STABLE_FILE
         with open(stable_file, "w"):
@@ -46,9 +46,9 @@ def _create_fake_rollout_parquet_file(path: Path, steps: list[int], num_files: i
 
 @pytest.fixture
 def fake_rollout_files_dir(tmp_path):
-    def _create(steps: list[int] = [0], num_files: int = 1, batch_size: int = 1):
+    def _create(steps: list[int] = [0], num_files: int = 1, batch_size: int = 1, seq_len: int = 10):
         os.makedirs(tmp_path, exist_ok=True)
-        _create_fake_rollout_parquet_file(tmp_path, steps, num_files, batch_size)
+        _create_fake_rollout_parquet_file(tmp_path, steps, num_files, batch_size, seq_len)
         return tmp_path
 
     return _create
