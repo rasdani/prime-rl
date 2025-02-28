a86baf09986008efcc2ba4a8b194983f03eab53f
Sami Jaghouar <sami.jaghouar@gmail.com>
2025-02-28 22:05:07 +0000

    update pad token id for qwen
    Signed-off-by: Sami Jaghouar <sami.jaghouar@gmail.com>

--- src/zeroband/models.py
+++ src/zeroband/models.py
@@ -43,5 +43,12 @@ def get_model_and_tokenizer(model_name: ModelName) -> tuple[torch.nn.Module, Aut
     tokenizer = AutoTokenizer.from_pretrained(name_to_hf_tokenizer[model_name])
     config_model = config_class.from_pretrained(name_to_hf_model[model_name], attn_implementation="flex_attention")
     model = model_class.from_pretrained(pretrained_model_name_or_path=name_to_hf_model[model_name], config=config_model)
-    tokenizer.pad_token_id = tokenizer.eos_token_id
+
+    if isinstance(model, Qwen2ForCausalLM):
+        tokenizer.pad_token_id = (
+            tokenizer.eod_id
+        )  # inspired from https://github.com/QwenLM/Qwen/blob/f7e3e7cb774b8667dde469250ac8e38ce850db1c/finetune.py#L328C1-L329C1
+    else:
+        tokenizer.pad_token_id = tokenizer.eos_token_id
+
     return model, tokenizer  # type: ignore
