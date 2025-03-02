aca94b23e83b825e87e641b82bb8f323735e231e
Sami Jaghouar <sami.jaghouar@gmail.com>
2025-03-02 00:58:31 +0000

    fix cache torch compile
    Signed-off-by: Sami Jaghouar <sami.jaghouar@gmail.com>

--- src/zeroband/models.py
+++ src/zeroband/models.py
@@ -44,4 +44,6 @@ def get_model_and_tokenizer(model_name: ModelName) -> tuple[ModelType, AutoToken
     config_model = config_class.from_pretrained(name_to_hf_model[model_name], attn_implementation="flex_attention")
     model = model_class.from_pretrained(pretrained_model_name_or_path=name_to_hf_model[model_name], config=config_model)
     tokenizer.pad_token_id = tokenizer.eos_token_id
-    return model, tokenizer  # type: ignore
+    model.generation_config.cache_implementation = "static"
+
+    return model, tokenizer
