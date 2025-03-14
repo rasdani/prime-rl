b8ff4f9a0dbacb4125e12d7f9198eaac32067a1e
Jackmin801 <ongjackm@gmail.com>
2025-03-14 08:15:18 +0000

    fix: no decode seqs

--- src/zeroband/inference.py
+++ src/zeroband/inference.py
@@ -145,7 +145,7 @@ def get_parquet_table(
             output_logprobs_list.append(get_own_logprobs(output.logprobs))
             advantages_list.append(adv)
             rewards_list.append(reward)
-            proofs_list.append(next(proof_iter))
+            proofs_list.append(next(proof_iter) if len(output.token_ids) > 1 else b"")
             steps_list.append(step)
 
     arrays = [
