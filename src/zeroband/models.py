4bf30099a9d7241778ee5aa0af66630a3394f917
Sami Jaghouar <sami.jaghouar@gmail.com>
2025-03-20 18:01:47 +0000

    use pad video token
    Signed-off-by: Sami Jaghouar <sami.jaghouar@gmail.com>

--- src/zeroband/models.py
+++ src/zeroband/models.py
@@ -51,5 +51,10 @@ def get_model_and_tokenizer(model_name: ModelName, attn_impl: AttnImpl) -> tuple
     config_model = config_class.from_pretrained(name_to_hf_model[model_name], attn_implementation=attn_impl)
     config_model.use_cache = False
     model = model_class.from_pretrained(pretrained_model_name_or_path=name_to_hf_model[model_name], config=config_model)
-    tokenizer.pad_token_id = tokenizer.eos_token_id
+
+    if model_class == Qwen2ForCausalLM:
+        tokenizer.pad_token_id = 151656  # video_pad token id
+    else:
+        tokenizer.pad_token_id = tokenizer.eos_token_id
+
     return model, tokenizer  # type: ignore
