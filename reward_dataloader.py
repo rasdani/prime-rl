6cf49b2c9a7d338c026af466381e3f3f3f305ba8
Sami Jaghouar <sami.jaghouar@gmail.com>
2025-03-21 18:05:36 +0000

    fix dummy script
    Signed-off-by: Sami Jaghouar <sami.jaghouar@gmail.com>

--- reward_dataloader.py
+++ reward_dataloader.py
@@ -1,4 +1,6 @@
+import os
 from pydantic_config import parse_argv
+import torch
 import torch.distributed as dist
 from transformers import AutoTokenizer
 from zeroband.training.data import get_dataloader
@@ -30,35 +32,69 @@ def main(config: Config):
             avg_rewards = 0
             non_masked_avg_rewards = 0
             mask_avg_rollout = 0
+            mask_bool_rollout = 0
+            rewards_bool = 0
+            proper_rewards_rollout = 0
 
         batch_rewards = 0
         non_masked_rewards = 0
         mask_avg = 0
+        mask_bool_avg = 0
+        batch_rewards_bool = 0
+
+        proper_rewards_sum = torch.tensor(0.0)
+        proper_rewards_token_count = torch.tensor(0.0)
 
         for j in range(gradient_accumulation_steps):
             batch = next(train_dataloader_iterator)
             rewards = batch["rewards"]
             mask = batch["loss_mask"].float()
+            mask_bool = batch["loss_mask"].bool()
             mask_avg += mask.mean() / gradient_accumulation_steps
+            mask_bool_avg += mask_bool.float().mean() / gradient_accumulation_steps
             batch_rewards += (rewards * mask).mean() / gradient_accumulation_steps
+            batch_rewards_bool += (rewards[mask_bool]).mean() / gradient_accumulation_steps
             non_masked_rewards += rewards.mean() / gradient_accumulation_steps
+
+            r = rewards[mask_bool]
+
+            proper_rewards_sum += r.sum()
+            proper_rewards_token_count += r.numel()
 
         batch_rewards = batch_rewards / world_info.world_size
         non_masked_rewards = non_masked_rewards / world_info.world_size
         mask_avg = mask_avg / world_info.world_size
+        mask_bool_avg = mask_bool_avg / world_info.world_size
+        batch_rewards_bool = batch_rewards_bool / world_info.world_size
+
+        dist.all_reduce(proper_rewards_sum, op=dist.ReduceOp.SUM)
+        dist.all_reduce(proper_rewards_token_count, op=dist.ReduceOp.SUM)
+        proper_rewards = proper_rewards_sum / proper_rewards_token_count
+
         dist.all_reduce(batch_rewards, op=dist.ReduceOp.SUM)
         dist.all_reduce(non_masked_rewards, op=dist.ReduceOp.SUM)
         dist.all_reduce(mask_avg, op=dist.ReduceOp.SUM)
-
+        dist.all_reduce(mask_bool_avg, op=dist.ReduceOp.SUM)
+        dist.all_reduce(batch_rewards_bool, op=dist.ReduceOp.SUM)
         avg_rewards += batch_rewards / config.optim.step_per_rollout
         non_masked_avg_rewards += non_masked_rewards / config.optim.step_per_rollout
         mask_avg_rollout += mask_avg / config.optim.step_per_rollout
-
+        mask_bool_rollout += mask_bool_avg / config.optim.step_per_rollout
+        rewards_bool += batch_rewards_bool / config.optim.step_per_rollout
+        proper_rewards_rollout += proper_rewards / config.optim.step_per_rollout
         step += 1
 
+        if os.environ.get("RANK") == "0":
+            print(f"step {step} , proper rewards: {proper_rewards:.4f}, avg_rewards: {avg_rewards:.4f}")
         if step % config.optim.step_per_rollout == 0:
             print(
-                f"[rank {world_info.rank}] step {step} rewards: {avg_rewards:.4f}, non-masked rewards: {non_masked_avg_rewards:.4f}, mask avg: {mask_avg_rollout:.4f}"
+                f"[rank {world_info.rank}] step {step}\n"
+                f"rewards: {avg_rewards:.4f}\n"
+                f"non-masked rewards: {non_masked_avg_rewards:.4f}\n"
+                f"rewards bool: {rewards_bool:.4f}\n"
+                f"mask avg: {mask_avg_rollout:.4f}\n"
+                f"mask bool avg: {mask_bool_rollout:.4f}\n"
+                f"proper rewards: {proper_rewards_rollout:.4f}\n"
             )
 
         if step >= config.optim.total_steps:
