59b273a01ef1c1aca7c9464f1d74a39a37acb239
apaz-cli <aarpazdera@gmail.com>
2025-04-01 00:41:54 +0000

    Remove ParallelAwareDataloader

--- src/zeroband/training/data.py
+++ src/zeroband/training/data.py
@@ -253,68 +253,6 @@ class ParquetDataset(IterableDataset):
                     break
 
 
-class ParallelAwareDataloader(StatefulDataLoader):
-    """Dataloader that is aware of distributed data parallelism.
-
-    This dataloader is used to load data in a distributed data parallel fashion. It also
-    utilizes ``torchdata.stateful_dataloader.StatefulDataLoader`` to implement the necessary
-    methods such as ``__iter__``.
-
-    Args:
-        dataset (IterableDataset): The dataset to iterate over.
-        dp_rank: Data parallelism rank for this dataloader.
-        dp_world_size: The world size of the data parallelism.
-        batch_size: The batch size to use for each iteration.
-    """
-
-    dp_rank: int
-    dp_world_size: int
-    batch_size: int
-
-    def __init__(
-        self,
-        dataset: IterableDataset,
-        batch_size: int,
-        dp_rank: int,
-        dp_world_size: int,
-        **kwargs,
-    ):
-        self.dp_world_size = dp_world_size
-        self.dp_rank = dp_rank
-        self.batch_size = batch_size
-        super().__init__(dataset, batch_size, **kwargs)
-        self._rank_id = f"dp_rank_{dp_rank}"
-
-    def state_dict(self) -> dict[str, Any]:
-        # Store state only for dp rank to avoid replicating the same state across other dimensions.
-        return {
-            # We don't have to use pickle as DCP will serialize the state_dict. However,
-            # we have to keep this for backward compatibility.
-            self._rank_id: pickle.dumps(super().state_dict()),
-            "world_size": self.dp_world_size,
-        }
-
-    def load_state_dict(self, state_dict: dict[str, Any]) -> None:
-        # State being empty is valid.
-        if not state_dict:
-            return
-
-        if self._rank_id not in state_dict:
-            print(
-                f"DataLoader state is empty for dp rank {self.dp_rank}, "
-                "expected key {self._rank_id}"
-            )
-            return
-
-        assert self.dp_world_size == state_dict["world_size"], (
-            "dp_degree is inconsistent before and after checkpoint, "
-            "dataloader resharding is not supported yet."
-        )
-        # We don't have to use pickle as DCP will serialize the state_dict. However, we have to
-        # keep this for backward compatibility.
-        super().load_state_dict(pickle.loads(state_dict[self._rank_id]))
-
-
 class BatchOutput(TypedDict):
     input_ids: Int[torch.Tensor, "batch seq"]
     advantages: Float[torch.Tensor, "batch seq"]
@@ -399,4 +337,4 @@ def get_dataloader(
         train_dataset = ParquetDataset(Path(path), batch_size, data_config.timeout, dp_rank=dp_rank, dp_world_size=dp_world_size)
 
     collate_fn = PaddingColate(data_config.seq_length, tokenizer.pad_token_id)  # todo adjust padding token for qwen later
-    return ParallelAwareDataloader(train_dataset, batch_size=micro_batch_size, num_workers=data_config.num_workers, collate_fn=collate_fn, dp_rank=dp_rank, dp_world_size=dp_world_size), prefetcher
+    return StatefulDataLoader(train_dataset, batch_size=micro_batch_size, num_workers=data_config.num_workers, collate_fn=collate_fn), prefetcher
