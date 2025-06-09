93e9d351ba515cd8e89742b4889f8567284924ed
rasdani <73563550+rasdani@users.noreply.github.com>
2025-06-09 11:33:27 +0000

    add git diff reward

--- src/zeroband/inference/genesys/__init__.py
+++ src/zeroband/inference/genesys/__init__.py
@@ -4,6 +4,7 @@ from zeroband.inference.genesys.ascii_tree_formatting import compute_reward as c
 from zeroband.inference.genesys.code import evaluate_code
 from zeroband.inference.genesys.code_output_prediction import verify_code_output_prediction
 from zeroband.inference.genesys.complex_json_output import verify_complex_json_formatting
+from zeroband.inference.genesys.git_diff import compute_git_diff_reward
 from zeroband.inference.genesys.ifeval import verify_ifeval
 from zeroband.inference.genesys.math import compute_math_reward
 from zeroband.inference.genesys.pydantic_json_adherance import validate_pydantic_json
@@ -22,6 +23,7 @@ TaskType = Literal[
     "pydantic_adherance",
     "ifeval",
     "complex_json_output",
+    "git_diff",
 ]
 
 
@@ -43,4 +45,5 @@ _REWARD_FUNCTIONS: dict[TaskType, Callable] = {
     "pydantic_adherance": validate_pydantic_json,
     "ifeval": verify_ifeval,
     "complex_json_output": verify_complex_json_formatting,
+    "git_diff": compute_git_diff_reward,
 }
